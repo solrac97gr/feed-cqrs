bb3ea7c7b46e09d6448468741d39f47b9a2cdae7
carlos97gr <carlosgperu97@gmail.com>
2022-08-26 20:03:02 +0200

    Implemented EventStore

--- events/event.go
+++ events/event.go
@@ -9,7 +9,7 @@ type EventStore interface {
 	Close()
 	PublishCreatedFeed(ctx context.Context, feed *models.Feed) error
 	SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error)
-	OnCreatedFeed(f func(CreatedFeedMessage)) error
+	OnCreateFeed(f func(CreatedFeedMessage)) error
 }
 
 var eventStore EventStore
@@ -26,6 +26,6 @@ func SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error
 	return eventStore.SubscribeCreatedFeed(ctx)
 }
 
-func OnCreatedFeed(f func(CreatedFeedMessage)) error {
-	return eventStore.OnCreatedFeed(f)
+func OnCreateFeed(f func(CreatedFeedMessage)) error {
+	return eventStore.OnCreateFeed(f)
 }
