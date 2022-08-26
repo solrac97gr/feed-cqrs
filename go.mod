fa58a57971b1762ef447052dd942c9fddff36921
carlos97gr <carlosgperu97@gmail.com>
2022-08-26 17:58:55 +0200

    repository created

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/solrac97gr/feed-cqrs
+
+go 1.18
---
+++ repository/repository.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"context"
+	"github.com/solrac97gr/feed-cqrs/models"
+)
+
+type Repository interface {
+	Close()
+	InsertFeed(ctx context.Context, feed *models.Feed) error
+	ListFeeds(ctx context.Context) ([]*models.Feed, error)
+}
+
+var repository Repository
+
+func SetRepository(r Repository) {
+	repository = r
+}
+
+func Close() {
+	repository.Close()
+}
+
+func InsertFeed(ctx context.Context, feed *models.Feed) error {
+	return repository.InsertFeed(ctx, feed)
+}
+
+func ListFeeds(ctx context.Context) ([]*models.Feed, error) {
+	return repository.ListFeeds(ctx)
+}
