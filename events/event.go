d8941bb43b3d1138a7c09dde3523f4b06764ae86
carlos97gr <carlosgperu97@gmail.com>
2022-08-26 19:19:04 +0200

    Messages and Event interfaces created

---
+++ events/event.go
@@ -0,0 +1,31 @@
+package events
+
+import (
+	"context"
+	"github.com/solrac97gr/feed-cqrs/models"
+)
+
+type EventStore interface {
+	Close()
+	PublishCreatedFeed(ctx context.Context, feed *models.Feed) error
+	SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error)
+	OnCreatedFeed(f func(CreatedFeedMessage)) error
+}
+
+var eventStore EventStore
+
+func Close() {
+	eventStore.Close()
+}
+
+func PublishCreatedFeed(ctx context.Context, feed *models.Feed) error {
+	return eventStore.PublishCreatedFeed(ctx, feed)
+}
+
+func SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error) {
+	return eventStore.SubscribeCreatedFeed(ctx)
+}
+
+func OnCreatedFeed(f func(CreatedFeedMessage)) error {
+	return eventStore.OnCreatedFeed(f)
+}
